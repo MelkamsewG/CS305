01e26e17e4cabe5dcd93fae034895bd6421bcdf1
MelkamsewG <melkamsewu@gmail.com>
2021-08-31 22:45:17 -0500

    text

---
+++ w2d2/func.js
@@ -0,0 +1,15 @@
+/*
+Write a function named testPrime that returns true when the argument to the 
+function is a prime number, otherwise returns false.
+• Now call the function to test if user input is prime or not
+*/
+'use strict';
+
+function testPrime(num) {
+    for (let i = 2; i < num; i++) {
+
+    }
+    // if(num) return
+    if (num > 2 && )
+
+}
