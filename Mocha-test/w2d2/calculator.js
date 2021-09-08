b658421a91c599936cf0c42705506a409549d84f
MelkamsewG <melkamsewu@gmail.com>
2021-09-08 01:22:54 -0500

    w2d2

--- Mocha-test/w2d2/calculator.js
+++ Mocha-test/w2d2/calculator.js
@@ -1,14 +1,49 @@
 "use strict";
 
-const calculator = {};  // implement this
+//const calculator = {}; // implement this
 
+/**
+ *  * @returns {Calculator} constructor function
+ */
+function Calculator() {
+    this.setValue = function Calculator(a, b) {
+            this.a = a;
+            this.b = b;
+        }
+        /**
+         * 
+         * @returns{Number} sum;
+         */
+    this.sum = function() {
+        return this.a + this.b;
+    };
 
+    /**
+     * 
+     * @returns {Number} multiplication;
+     */
+    this.mul = function() {
+            return this.a * this.b;
+        }
+        /**
+         * 
+         * @returns {Number} subtraction;
+         */
+    this.sub = function() {
+            return this.a - this.b
+        }
+        /**
+         * 
+         * @returns {Number} division;
+         */
+    this.div = function() {
+        return this.a / this.b;
+    }
+};
+module.exports = { Calculator };
 
-
-
-
-
-
-/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
-/* must be at end of file if are exporting an object so the export is after the definition */
-module.exports = {calculator }; //add all of your object names here that you need for the node mocha tests
+/* You need the module.exports when testing in node.  
+Comment it out when you send your file to the browser */
+/* must be at end of file if are exporting an object so 
+the export is after the definition */
+module.exports = { calculator }; //add all of your object names here that you need for the node mocha tests
