5afd423dc4236604d120e52595e20bc79840954f
MelkamsewG <melkamsewu@gmail.com>
2021-09-14 14:08:59 -0500

    w2d6

---
+++ Mocha-test/w2d2/findIndex.js
@@ -0,0 +1,28 @@
+"use strict"
+// let arr = [7, 5, 4, 6, 9, 8, 6];
+
+// console.log(arr.findIndex(item => item % 2 == 0));
+
+// let lengths = ["Bilbo", "Gandalf", "Nazgul"].map(item => item.length);
+// console.log(lengths); // [5, 7, 6]
+// console.log(arr.sort()); // [4, 5, 6, 6, 7, 8, 9]
+// console.log(arr.reverse()); //  [9, 8, 7, 6, 6, 5, 4]
+
+// let arr2 = ["Melkamu", "Mellese", "Kumalo"];
+// console.log(arr2.reverse()); // ['Kumalo', 'Mellese', 'Melkamu']
+let myarr = [];
+let number = '12345';
+let result = number.split("");
+//for (let i = number.length - 1; i >= 0; i--) {
+    // myarr.push(Number(number[i].join('')));
+
+}
+myarr.join(',')
+console.log(myarr);
+
+
+
+// myarr.push(result);
+// console.log(myarr);
+// let newNum = Math.floor(number);
+// console.log(newNum);
---
+++ Mocha-test/w3d1/stack.js
@@ -0,0 +1,51 @@
+"use strict"
+
+// function funX(a, b) {
+//     let c;
+//     c = 5;
+//     funY(a * c, "yes");
+// }
+
+// function funY(x, y) {
+//     let z;
+//     z = "I can see the sea";
+//     console.log("What is on the stack here?");
+// }
+
+// function main() {
+//     let a;
+//     let b;
+//     a = "Hello";
+//     funX(3, a);
+//     b = "World";
+// }
+// main();
+
+// function pow(x, n) {
+//     let result = 1;
+//     // multiply result by x n times in the loop
+//     for (let i = 1; i <= n; i++) {
+//         result *= x;
+//     }
+//     //return result;
+//     console.log(result);
+
+// }
+// pow(2, 3);
+
+
+// function fib(n) {
+//     return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+// }
+// alert(fib(3)); // 2
+// alert(fib(7)); // 13
+// //alert(fib(77));
+// // fib(77); // will be extremely slow!
+
+function findLengthRecursive(s) {
+    if (s === "")
+        return 0;
+    else
+        return 1 + findLengthRecursive(s.substr(1));
+}
+console.log(findLengthRecursive("Melkamu"));
